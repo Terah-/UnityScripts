e52677fce823e072acddb3cc7ed47edc0db504ae
Terah <terah.dev@gmail.com>
2016-01-01 23:24:32 +0100

    Oscillator

---
+++ README.md
@@ -0,0 +1,4 @@
+Some scripts that I often use and might be usefull to other people.
+Feel free to use them in your projets !
+
+Terah.
---
+++ Scripts/Oscillator.cs
@@ -0,0 +1,40 @@
+﻿/*
+ * Terah
+ * https://github.com/Terah-
+ */
+
+using UnityEngine;
+
+public class Oscillator
+{
+    private float time = 0f;
+
+    // In oscillations per second
+    public float Frequency;
+
+    public Oscillator()
+    {
+        Frequency = 1f;
+    }
+
+    public Oscillator(float frequency)
+    {
+        Frequency = frequency;
+    }
+
+    private void Update()
+    {
+        time += Time.deltaTime;
+    }
+
+    public float GetValue()
+    {
+        Update();
+        return (Mathf.Cos(time*Frequency*Mathf.PI*2) + 1f)/2f;
+    }
+
+    public float GetValue(float min, float max)
+    {
+        return Mathf.Lerp(min, max, GetValue());
+    }
+}
